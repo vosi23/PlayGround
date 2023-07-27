58c1210296dc03abee9cf897e21b4e5e287f2e72
vosi23 <vasyle30@gmail.com>
2023-07-27 22:44:10 +0300

    First commit with snake terminal minigame

---
+++ terminal_snake/display/display.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "display.h"
+
+void display_menu(void)
+{
+    printf("####################");
+}
+
+bool display_init(void)
+{
+    /* TODO: CREATE DEFINES EITHER USE MALLOC */
+    char template[15][45];
+    uint8_t i, j;
+    i = j = 0u;
+
+    FILE *fTemplate;
+    fTemplate = fopen("menu_template", "r");
+
+    if(fTemplate == NULL)
+        goto EXIT;
+    
+    while( (template[i][j] = fgetc(fTemplate)) != EOF )
+    {
+        printf("%c", template[i][j]);
+    
+        if(template[i][j] == '\n')
+        {
+            i++;
+            j=0;
+        }
+        else
+            j++;  
+    }
+    
+    fclose(fTemplate);
+    printf("\n\n");
+    return true;
+
+EXIT:
+    printf("Error handling display_init function!\n");
+    return false; 
+}
---
+++ terminal_snake/main.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include "main.h"
+#include "display/display.h"
+
+/* TODO: REMOVE IT */
+#include "stdio.h"
+/* TODO: REMOVE IT */
+
+/* TODO: Create makefile build env */
+
+void main_initGame(void);
+
+uint8_t main(void)
+{
+    main_initGame();
+
+    return 0u;
+}
+
+void main_initGame(void)
+{
+    printf("%d", display_init());
+}
